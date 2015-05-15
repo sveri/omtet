590a719347a64473888887e1e30053a77b83b270
sveri <sveri80@gmail.com>
2015-05-15 22:20:43 +0200

    cleanup

--- test/de/sveri/omtet/service/fake_tetris_test.clj
+++
@@ -1,6 +0,0 @@
-(ns de.sveri.omtet.service.fake-tetris-test
-  (:require [de.sveri.omtet.service.fake-tetris :as fk]
-            [clojure.test :refer :all]))
-
-(deftest two-cols
-  (is (= [[0 0] [0 1] [0 3]] (fk/doit [[0 1] [1 1] [3 3]] 2 2))))

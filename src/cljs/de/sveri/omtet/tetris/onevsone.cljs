ff6b7669587f7901e8aba9b5b55a14b12d131890
sveri <sveri@sveri.de>
2015-06-09 23:18:23 +0200

    more buttons

--- src/cljs/de/sveri/omtet/tetris/onevsone.cljs
+++ src/cljs/de/sveri/omtet/tetris/onevsone.cljs
@@ -3,6 +3,19 @@
             [reagent.core :as reagent]
             [re-frame.core :as rf]))
 
+(defn tetr-div [idx width]
+  (let [tet-width "200px"
+        score (rf/subscribe [:score])]
+    [:div {:class (str "col-md-" width)}
+     [:canvas {:id (str "tetris-canv-" idx) :height "400px" :width tet-width :style {:background-color "#444444"}}]
+     [:div {:id (str "score-" idx) :style {:background-color "#CCCCCC" :width tet-width}} (str "Score: "
+                                                                                               ;@score
+                                                                                               )]
+     [:br]
+     (if (= 1 idx) [:button.btn.btn-primary {:on-click #(println "foo")} "Ready"] identity)
+
+     ]))
+
 (defn core []
   [:div "ae"]
   (let [tet-width "200px"
@@ -10,29 +23,24 @@
         score (rf/subscribe [:score])
         ]
     ;(if initalized?
-      [:div.row
-       [:div.col-md-6
-        [:canvas#tetris-canv {:height "400px" :width tet-width :style {:background-color "#444444"}}]
-        ;[:div#score {:style {:background-color "#CCCCCC" :width tet-width}} (str "Score: " @score)]
-        ]
-       [:div.col-md-6
-        [:canvas#tetris-canv {:height "400px" :width tet-width :style {:background-color "#444444"}}]
-        ;[:div#score {:style {:background-color "#CCCCCC" :width tet-width}} (str "Score: " @score)]
-        ]]
-  ;     [:div.col-md-3
-  ;      (let [started? @(rf/subscribe [:started?])
-  ;            paused? @(rf/subscribe [:paused?])]
-  ;        [:div
-  ;         [:button.btn.btn-primary
-  ;          {:on-click (cond (not started?) #(rf/dispatch [:start-game])
-  ;                           paused? #(rf/dispatch [:unpause-game])
-  ;                           (not paused?) #(rf/dispatch [:pause-game]))}
-  ;          (cond (not started?) "Start" paused? "Continue" (not paused?) "Pause")]
-  ;         [:br]
-  ;         [:button.btn.btn-primary {:on-click #(rf/dispatch [:restart-game])} "Restart"]])]]
-  ;
-  ;    [:h4 "Loading..."]
-      ;)
+    [:div.row
+     (tetr-div 1 6)
+     (tetr-div 2 6)
+     ]
+    ;     [:div.col-md-3
+    ;      (let [started? @(rf/subscribe [:started?])
+    ;            paused? @(rf/subscribe [:paused?])]
+    ;        [:div
+    ;         [:button.btn.btn-primary
+    ;          {:on-click (cond (not started?) #(rf/dispatch [:start-game])
+    ;                           paused? #(rf/dispatch [:unpause-game])
+    ;                           (not paused?) #(rf/dispatch [:pause-game]))}
+    ;          (cond (not started?) "Start" paused? "Continue" (not paused?) "Pause")]
+    ;         [:br]
+    ;         [:button.btn.btn-primary {:on-click #(rf/dispatch [:restart-game])} "Restart"]])]]
+    ;
+    ;    [:h4 "Loading..."]
+    ;)
     )
   )
 
